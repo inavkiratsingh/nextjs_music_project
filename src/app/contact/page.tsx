14dba483c762a06e24e79c4e6d35d242807ab5de
Nav <navs6164@gmail.com>
2024-07-01 23:43:25 +0530

    pp

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,9 @@
 'use client';
 
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent } from 'react';
 import { BackgroundBeams } from '@/components/ui/background-beams';
 
 function contact() {
-
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('Submitted:', { email, message });
-  };
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
@@ -31,18 +23,17 @@ function contact() {
         programs, or events. Reach out and let us know how we can assist you
         in your musical journey.
       </p>
-      <form onSubmit={handleSubmit} className="space-y-4 mt-4">
+      <form className="space-y-4 mt-4">
         <input
           type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          // onChange={(e) => setEmail(e.target.value)}
           placeholder="Your email address"
           className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
           required
         />
         <textarea
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          // value={message}
+          // onChange={(e) => setMessage(e.target.value)}
           placeholder="Your message"
           className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
           rows={5}
